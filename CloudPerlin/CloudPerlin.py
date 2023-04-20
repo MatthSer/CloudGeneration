5c6c0d26311b39c3281f10ab021afbc341c17216
MatthSer <matthieu.serfaty@icloud.com>
2023-04-20 10:59:05 +0200

    v1

---
+++ CloudPerlin/CloudPerlin.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+
+def translation_mask(mask, dx, dy, shape_out):
+    h, w = mask.shape[:2]
+    dx = int(dx)
+    dy = int(dy)
+    ts_mat = np.array([[1, 0, dx], [0, 1, dy]])
+
+    translated_mask = np.zeros(shape_out)
+    for i in range(h):
+        for j in range(w):
+            origin_x = j
+            origin_y = i
+            origin_xy = np.array([origin_x, origin_y, 1])
+
+            new_xy = np.dot(ts_mat, origin_xy)
+            new_x = new_xy[0]
+            new_y = new_xy[1]
+
+            if 0 < new_x < w and 0 < new_y < h:
+                translated_mask[new_y, new_x] = mask[i, j]
+
+    return translated_mask
+
+
+def generate_perlin_noise_2d(shape, res):
+    def f(t):
+        return 6 * t ** 5 - 15 * t ** 4 + 10 * t ** 3
+
+    delta = (res[0] / shape[0], res[1] / shape[1])
+    d = (shape[0] // res[0], shape[1] // res[1])
+    grid = np.mgrid[0:res[0]:delta[0], 0:res[1]:delta[1]].transpose(1, 2, 0) % 1
+    # Gradients
+    angles = 2 * np.pi * np.random.rand(int(res[0]) + 1, int(res[1]) + 1)
+    gradients = np.dstack((np.cos(angles), np.sin(angles)))
+    g00 = gradients[0:-1, 0:-1].repeat(d[0], 0).repeat(d[1], 1)
+    g10 = gradients[1:, 0:-1].repeat(d[0], 0).repeat(d[1], 1)
+    g01 = gradients[0:-1, 1:].repeat(d[0], 0).repeat(d[1], 1)
+    g11 = gradients[1:, 1:].repeat(d[0], 0).repeat(d[1], 1)
+    # Ramps
+    n00 = np.sum(grid * g00, 2)
+    n10 = np.sum(np.dstack((grid[:, :, 0] - 1, grid[:, :, 1])) * g10, 2)
+    n01 = np.sum(np.dstack((grid[:, :, 0], grid[:, :, 1] - 1)) * g01, 2)
+    n11 = np.sum(np.dstack((grid[:, :, 0] - 1, grid[:, :, 1] - 1)) * g11, 2)
+    # Interpolation
+    t = f(grid)
+    n0 = n00 * (1 - t[:, :, 0]) + t[:, :, 0] * n10
+    n1 = n01 * (1 - t[:, :, 0]) + t[:, :, 0] * n11
+
+    return np.sqrt(2) * ((1 - t[:, :, 1]) * n0 + t[:, :, 1] * n1)
+
+
+def generate_fractal_noise_2d(shape, res, octaves=1, persistence=0.5):
+    noise = np.zeros(shape)
+    frequency = 1
+    amplitude = 1
+    for _ in range(octaves):
+        noise += amplitude * generate_perlin_noise_2d(shape, (frequency * res[0], frequency * res[1]))
+        frequency *= 2
+        amplitude *= persistence
+
+    return noise
+
+
+def cloud_generation(shape, res, octaves):
+    fractal_noise = generate_fractal_noise_2d(shape, res, octaves)
+    cloud = (fractal_noise - np.min(fractal_noise)) / (np.max(fractal_noise) - np.min(fractal_noise))
+
+    mask = (cloud > 0.1) * 255
+
+    return cloud, mask
+
+
+def cloud_copy(img, cloud):
+    cloud_blue = cloud
+    cloud_green = translation_mask(cloud, 5, 5, cloud.shape)
+    cloud_red = translation_mask(cloud, 10, 10, cloud.shape)
+    cloud = np.stack([cloud_red, cloud_green, cloud_blue], axis=2)
+
+    # noise
+    # noise = np.random.normal(0, 0.01, cloud.shape)
+    # cloud = cloud + noise
+
+    cloudy = (cloud * (2 ** 13)) * cloud + (1 - cloud) * img
+
+    return cloudy
--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import os
-import iio
+import imageio as iio
 import numpy as np
+import tifffile
+from CloudPerlin import CloudPerlin
+
 
 # if you need to access a file next to the source code, use the variable ROOT
 # for example:
 #    torch.load(os.path.join(ROOT, 'weights.pth'))
-ROOT = os.path.dirname(os.path.realpath(__file__))
 
-def main(input, output, sigma):
-    u = iio.read(input)
-    print("hello world", u.shape)
 
-    v = u + np.random.randn(*u.shape) * sigma
+def main(input, res, octave):
+    u = tifffile.imread(input)
 
-    iio.write(output, v)
+    cloud, mask = CloudPerlin.cloud_generation((1024, 1024), (res, res), octave)
+    cloudy = CloudPerlin.cloud_copy(u, cloud)
+
+    if not os.path.exists('output/'):
+        os.mkdir('output/')
+
+    tifffile.imwrite('output/cloud.png', cloud)
+    tifffile.imwrite('output/cloudy.png', cloudy.astype(np.uint16))
+
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=str, required=True)
-    parser.add_argument("--sigma", type=float, required=True)
-    parser.add_argument("--output", type=str, required=True)
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', dest='input', type=str, required=True, help='input image')
+    parser.add_argument('-r', '--res', dest='res', type=int, default=2, help="res noise parameter")
+    parser.add_argument('-o', '--octave', dest='octave', type=int, default=7, help="octave noise parameter")
     args = parser.parse_args()
-    main(args.input, args.output, args.sigma)
+
+    main(args.input, args.res, args.octave)
